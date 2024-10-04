d36048d5b8b709bb29164de3b5b345cef892e87d
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-04 16:10:16 +0530

    Add files via upload

---
+++ Ann.py
@@ -0,0 +1,48 @@
+#print("hello world")
+#print("hello\nWorld")
+'''
+if 5>2:
+ print("Five is greater than two")
+if 5>2:
+    print("Five is greater than two")
+if 5>2:
+         print("five is greater than two")
+x = 5
+x = "python"
+print(x)
+x = int(3)
+y = int(3)
+z = float(3)
+print (x)
+print (y)
+print(z)
+x = 5j
+y = "john"
+print(type(x))
+print(type(y))
+a = 4
+A = "JOHN"
+print (a)
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+x = "python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+username = input ("Enter username : ")
+print("username is : " + username)
+'''
+'''
+a = ("hello ")
+b = ("world ")
+c = a + b
+print(c)
+'''
+a = "Hello, world "
+print(a[1])
+
+b = "Hello, world "
+print(b[:5])
