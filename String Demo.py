7b0ff1fd55ab1d7ceaad00f4af434c481dc3aa6d
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-18 15:10:56 +0530

    Add files via upload
    A Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.

---
+++ String Demo.py
@@ -0,0 +1,10 @@
+'''
+Author:Ann Mariya Tony
+Date:18-10-2028
+Description:a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var1= "Hello World"
+str_var2=str_var1[6:12]
+print(str_var2)
+str_var3=" Everyone!"
+print(str_var2+str_var3)
