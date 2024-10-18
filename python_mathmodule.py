414e53b313abb891fe344c43b90f8650fd12a211
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-18 16:25:12 +0530

    Add files via upload
    A Python program that uses functions from the math module

---
+++ python_mathmodule.py
@@ -0,0 +1,14 @@
+'''
+Author:Ann mariya tony
+Date: 18-10-2024
+Description:a Python program that uses functions from the math module
+'''
+import math
+number=int(input ("Enter a number:"))
+square_root=math.sqrt(number)
+print("Square root of :" , number,":",square_root)
+Factorial= math.factorial(number)
+print("Factorial of :",number,":",Factorial)
+number2=int(input ("Enter a number:"))
+Power=math.pow(number,number2)
+print(number,"raised to  power",number2,":", Power)
