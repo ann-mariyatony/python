b1b95257000457480506122073f305fe419c424e
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-19 14:50:55 +0530

    Add files via upload
    A Python program that performs arithmetic operators.

---
+++ arithmetic operators.py
@@ -0,0 +1,20 @@
+'''
+Author : Ann Mariya Tony
+Date: 19-10-2024
+Description:A Python program that performs arithmetic operators.
+'''
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3:"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+subtract=num2-num1
+print("The difference when num2 is subtracted from num1 is",subtract)
+multiply=num1*num2
+print("The product of num 1 and num 2 is:",multiply)
+divide=num1/num2
+print("The quotient when num 1 is divided by num 2 is:",divide)
+remainder=num1%num2
+print("The remainder when num1 is divided by num2 is:", remainder)
+result = (num1 + num2) * num3 / 2
+print("The result of (num1 + num2) * num3 / 2 is:", result)
