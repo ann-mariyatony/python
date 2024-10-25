9ff027ececac0cdfc7560d8603347e4de7dfe584
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-25 15:15:00 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,12 @@
+'''
+Author:Ann mariya tony
+Date:25-10-2024
+Description:A Program to find a number positive or negative
+'''
+number=int(input("Enter first number:"))
+if(number>0):
+    print( number," is a positive number")
+elif(number<0):
+      print( number," is  a negative number")
+else:
+    print(number," is a zero")
