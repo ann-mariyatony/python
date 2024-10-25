cc17f8e4cb8a9bf44b6adcc153b906058c56a0fd
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-10-25 15:12:58 +0530

    Add files via upload

---
+++ largest number python.py
@@ -0,0 +1,11 @@
+'''
+Author:Ann mariya tony
+Date:25-10-2024
+Description:A Program to find greatest of 2 numbers
+'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter second number:"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
