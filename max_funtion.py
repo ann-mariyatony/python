46f951146f749d471af8e4479284e79fc0088482
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-30 15:10:40 +0530

    Add files via upload
    a Python function to find the maximum of three numbers.

---
+++ max_funtion.py
@@ -0,0 +1,12 @@
+'''
+Author:Ann mariya tony
+date:30-11-2024
+'''
+def max_numbers(list):
+    list.sort()
+    print("The maximum value is :",list[2])
+n1=int(input("Enter a number:"))
+n2 = int(input("enter a number:"))
+n3=int(input("Enter a number:"))
+list=[n1,n2,n3]
+max_numbers(list)
